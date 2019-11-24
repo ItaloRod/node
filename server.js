b486aa2d8ab0599c7701ce3852807697504b6618
Paulo Italo <pauloitalo@hotmail.com.br>
2019-11-24 16:07:04 -0300

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const requireDir = require('require-dir')
+
+// iniciando o app
+const app = express()
+app.use(express.json())
+
+//Inicialização do mongoDB
+try {
+    //colocar o ip do container do mongo
+    mongoose.connect(
+        "mongodb://172.22.0.1:27017/nodeapi?keepAlive=true&poolSize=30&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true})
+} catch (err) {
+    console.log(err)
+}
+
+requireDir('./src/models')
+
+// rotas
+app.use('/api', require('./src/routes'))
+
+// Ouve a porta 3000
+app.listen(3000)
